fe15804b1a51b6cbb959b690b8196d36b7c5d54b
Keisuke Nemura <nemuraystk@gmail.com>
2023-02-05 14:01:01 +0900

    update ヘッダー・フッター修正

--- src/components/utils/FooterComponent.tsx
+++ src/components/utils/FooterComponent.tsx
@@ -2,7 +2,7 @@ import { LinkComponent } from '@/components/utils/LinkComponent';
 
 const FooterComponent: React.FC = () => {
   return (
-    <div className='w-full h-24 flex items-center justify-center border-t border-gray-200 text-sm text-gray-400'>
+    <div className='w-full h-24 flex items-center justify-center border-t border-white text-sm text-black bg-white'>
       © 2023 - Present Sample site. All rights reserved.
     </div>
   );
