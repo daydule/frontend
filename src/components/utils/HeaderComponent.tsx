11c9563c9f5ae6e06460d931f8ae7811bc0e80c5
Keisuke Nemura <nemuraystk@gmail.com>
2023-02-05 23:31:48 +0900

    update headerのborder-color修正

--- src/components/utils/HeaderComponent.tsx
+++ src/components/utils/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import { IconContext } from 'react-icons/lib';
 
 const HeaderComponent: React.FC = () => {
   return (
-    <div className='w-full h-20 border-b border-black pl-8 flex items-center text-left text-3xl text-white bg-indigo-700'>
+    <div className='w-full h-20 border-b border-white pl-8 flex items-center text-left text-3xl text-white bg-indigo-700'>
       <IconContext.Provider value={{ size: '1.2em', className: 'text-white text-opacity-90' }}>
         <AiFillSchedule />
         <img src='/logo.png' className='ml-3' />
