9a8d9c373f3e683d2afc1ec159a01e9b6c293142
tom-takeru <tom.2461.ty@gmail.com>
2023-02-14 18:38:26 +0900

    update ゲストとして利用するボタンのマージン指定を修正

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -7,7 +7,7 @@ const LoginPage: NextPage = () => {
     <>
       <div className='container mt-28 mx-auto xl:w-2/5 w-full'>
         <LoginComponent />
-        <div className='my-6 text-center mx-auto xl:w-2/5 w-3/5'>
+        <div className='mt-6 text-center mx-auto xl:w-2/5 w-3/5'>
           {/* TODO: ゲスト利用のAPIが完了次第修正 */}
           <ButtonComponent type='button' text='ゲストとして利用する' />
         </div>
