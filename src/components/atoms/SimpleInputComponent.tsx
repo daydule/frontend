3ada5a7ba677e3375f416eb5fafb087dc1633d79
tom-takeru <tom.2461.ty@gmail.com>
2023-03-04 17:12:13 +0900

    DAYD-727 add SimpleInputComponent

---
+++ src/components/atoms/SimpleInputComponent.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import { IconContext } from 'react-icons/lib';
+
+type Props<T> = {
+  id: string;
+  name: string;
+  type?: 'text' | 'password' | 'number' | undefined;
+  value: T;
+  placeholder?: string;
+  extraClassName?: string | undefined;
+  setter: React.Dispatch<React.SetStateAction<T>>;
+};
+
+export const SimpleInputComponent = <T extends string | number>(props: Props<T>) => {
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.setter(event.target.value as T);
+  };
+
+  return (
+    <div className='relative'>
+      <input
+        id={props.id}
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        className={props.extraClassName + ' ' + 'w-full border border-black text-base block p-2.5 h-12'}
+        placeholder={props.placeholder}
+        onChange={inputHandler}
+      />
+    </div>
+  );
+};
