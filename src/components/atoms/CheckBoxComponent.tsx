16921e8db7fd706665b92c8565cb4f0c94f8c2b2
tom-takeru <tom.2461.ty@gmail.com>
2023-05-15 05:39:46 +0900

    DAYD-764 update CheckBoxComponentのレイアウト調整

--- src/components/atoms/CheckBoxComponent.tsx
+++ src/components/atoms/CheckBoxComponent.tsx
@@ -8,18 +8,18 @@ type Props = {
 
 export const CheckBoxComponent = (props: Props) => {
   return (
-    <div className='relative'>
+    <div className='flex'>
       <input
         id={props.id}
         name={props.name}
         type='checkbox'
         checked={props.value}
-        className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+        className='block w-4 h-4 my-auto text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
         onChange={props.handleChange}
       />
       <label
         htmlFor={props.id}
-        className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+        className='block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
         children={props.title}
       />
     </div>
