d7c51ce0e0e9866004e5fd74e8061f5c36bed578
tom-takeru <tom.2461.ty@gmail.com>
2023-05-21 18:09:11 +0900

    DAYD-801 add TODO更新モーダル

--- src/components/molecules/TodoCardComponent.tsx
+++ src/components/molecules/TodoCardComponent.tsx
@@ -1,5 +1,7 @@
 import { Plan } from '@/redux/types';
 import { DeletePlanButtonComponent } from '@/components/molecules/DeletePlanButtonComponent';
+import { useState } from 'react';
+import { UpdateTodoModalComponent } from './UpdateTodoModalComponent';
 
 type Props = {
   todo: Plan;
@@ -10,15 +12,26 @@ export const TodoCardComponent = (props: Props) => {
   const className =
     (props.isDragging ? 'w-full' : 'w-[calc(100%_-_2rem)] mx-4') +
     ' ' +
-    'h-16 px-4 my-2 rounded-md bg-indigo-300 shadow-lg text-white flex justify-between items-center';
+    'h-16 px-4 my-2 rounded-md bg-indigo-300 shadow-lg text-white flex justify-between items-center hover:w-[calc(100%_-_1rem)] hover:ml-0 duration-300';
+
+  const [showsModal, setShowsModal] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShowsModal(true);
+  };
+
+  const handleClose = () => {
+    setShowsModal(false);
+  };
 
   return (
-    <div className={className}>
+    <div className={className} onClick={handleClick}>
       <div className='text-center text-lg'>{props.todo.title}</div>
       <div className='text-center text-xl flex'>
         <div>{props.todo.processTime}分</div>
         <DeletePlanButtonComponent size={1.5} planId={props.todo.id} />
       </div>
+      {showsModal && <UpdateTodoModalComponent showsModal={showsModal} handleClose={handleClose} todo={props.todo} />}
     </div>
   );
 };
