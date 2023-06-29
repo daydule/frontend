95a88dfb0005e2e3a923c9614ec050a08d596b1b
tom-takeru <tom.2461.ty@gmail.com>
2023-06-29 23:45:58 +0900

    DAYD-786 refactor errorHandler

--- src/helpers/errorHandlerHelper.ts
+++ src/helpers/errorHandlerHelper.ts
@@ -3,13 +3,10 @@ import { toastr } from 'react-redux-toastr';
 
 export const errorHandler = (error: { data: ErrorResponse }) => {
   const errorResponse = error.data;
-  if (errorResponse?.isError) {
-    if (typeof errorResponse.errorMessage === 'string') {
-      toastr.error('エラー', errorResponse.errorMessage);
-    } else {
-      errorResponse.errorMessage.forEach((message) => {
-        toastr.error('エラー', message);
-      });
-    }
+  if (!errorResponse.isError) return;
+  if (typeof errorResponse.errorMessage === 'string') {
+    toastr.error('エラー', errorResponse.errorMessage);
+  } else {
+    errorResponse.errorMessage.forEach((message) => toastr.error('エラー', message));
   }
 };
