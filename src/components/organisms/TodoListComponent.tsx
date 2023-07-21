73d2b947719987d381cd57dec6e43a6b21b2518a
Kota Kawamoto <atok.reverse.1040@gmail.com>
2023-07-21 21:46:37 +0900

    DAYD-776 fix 不要なコンソール出力を削除

--- src/components/organisms/TodoListComponent.tsx
+++ src/components/organisms/TodoListComponent.tsx
@@ -29,7 +29,6 @@ export const TodoListComponent = () => {
   }, [scheduleReadResult]);
 
   const handleToggleTodoArea = () => {
-    console.log('toggleButtonClicked!');
     setIsExpand((prevState: boolean) => !prevState);
   };
 
